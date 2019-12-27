04934d20ef7cdee8182c01a115144100c112e0ef
rajiv_iyer <Rajiv.iyer@mastek.com>
2019-12-27 23:46:25 +0000

    Liquibase with Spring boot initial commit

---
+++ src/main/java/com/example/liquibaseprice/LiquiBasePriceApplication.java
@@ -0,0 +1,13 @@
+package com.example.liquibaseprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquiBasePriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquiBasePriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/liquibaseprice/LiquibasepriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liquibaseprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibasepriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
